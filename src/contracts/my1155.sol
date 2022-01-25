d87305ddff0ec85264756142ded7cb648ac66f58
bozodesign <bozoce@yahoo.co.th>
2022-01-25 20:15:15 +0700

    g

--- src/contracts/my1155.sol
+++ src/contracts/my1155.sol
@@ -14,7 +14,9 @@ contract GameItems is ERC1155 {
     uint256 public constant AKANE = 4;
     uint256 public constant SHEEPY = 5;
 
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+    mapping (uint256 => string) private _uris;
+
+    constructor() ERC1155("http://smc.alotof.fun/img/smcjson2/{id}.json") {
         _mint(msg.sender, GOLD, 10**9, "");
         _mint(msg.sender, SILVER, 10**18, "");
         _mint(msg.sender, AKIRA, 1, "");
@@ -24,7 +26,9 @@ contract GameItems is ERC1155 {
     }
 
     function uri(uint256 tokenId) override public view returns (string memory) {
-        return(_uris[tokenId]);
+        return(string(abi.encodePacked(
+            "ipfs://QmfZSNMMJ98HrcV9Wdpx2MJgCn8w1yxdK4TBuRUZJttvW9/",Strings.toString(tokenID),".json"
+        )));
     }
     
     function setTokenUri(uint256 tokenId, string memory uri) public onlyOwner {
